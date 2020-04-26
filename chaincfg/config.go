1b2d15ed263f8d4fa8f01cbee480819eb4d2c658
anweilaw <15990041298@163.com>
2020-04-26 11:43:11 +0800

    update cfg param code of mining block

--- chaincfg/config.go
+++ chaincfg/config.go
@@ -153,9 +153,9 @@ type FConfig struct {
 	DebugLevel           string        `short:"d" long:"debuglevel" description:"Logging level for all subsystems {trace, debug, info, warn, error, critical} -- You may also specify <subsystem>=<level>,<subsystem2>=<level>,... to set the logger level for individual subsystems -- Use show to list available subsystems"`
 	Upnp                 bool          `long:"upnp" description:"Use UPnP to map our listening port outside of NAT"`
 	MinTxPrice           float64       `long:"mintxprice" description:"The minimum transaction price, it should not less than 0.01"`
-	BlkProductedTimeOut  float64       `long:"blkproductedtimeout" description:"the value for the policy BlockProductedTimeOut,it's value must be in range of (0, 1)"`
-	TxConnectTimeOut     float64       `long:"txconnecttimeout" description:"the value for the policy TxConnectTimeOut,it's value must be in range of (0, 1)"`
-	UtxoValidateTimeOut  float64       `long:"utxovalidatetimeout" description:"the time for validating utxos,it's value must be in range of (0, 1)"`
+	BlkProductedTimeOut  float64       `long:"blkproductedtimeout" description:"the value for the policy BlockProductedTimeOut,the value must be in range of (0, 1)"`
+	TxConnectTimeOut     float64       `long:"txconnecttimeout" description:"the value for the policy TxConnectTimeOut,the value must be in range of (0, 1)"`
+	UtxoValidateTimeOut  float64       `long:"utxovalidatetimeout" description:"the time for validating utxos,the value must be in range of (0, 1)"`
 	MaxOrphanTxs         int           `long:"maxorphantx" description:"Max number of orphan transactions to keep in memory"`
 	MaxOrphanTxSize      int           `long:"maxorphantxsize" description:"Max size of an orphan transaction to allow in memory"`
 	Consensustype        string        `long:"consensustype" description:"Consensus type which the server uses"`
@@ -678,40 +678,40 @@ func LoadConfig() (*FConfig, []string, error) {
 		return nil, nil, err
 	}
 
-	if cfg.BlkProductedTimeOut < 0 || cfg.BlkProductedTimeOut >= 1 {
+	if cfg.BlkProductedTimeOut == 0 {
+		cfg.BlkProductedTimeOut = DefaultBlockProductedTimeOut
+	}
+	if cfg.BlkProductedTimeOut <= 0 || cfg.BlkProductedTimeOut >= 1 {
 		str := "%s: The BlkProductedTimeOut is out of range (0, 1) " +
-			"-- parsed [%d]"
+			"-- parsed [%f]"
 		err := fmt.Errorf(str, funcName, cfg.BlkProductedTimeOut)
 		fmt.Fprintln(os.Stderr, err)
 		fmt.Fprintln(os.Stderr, usageMessage)
 		return nil, nil, err
 	}
-	if cfg.BlkProductedTimeOut == 0 {
-		cfg.BlkProductedTimeOut = DefaultBlockProductedTimeOut
-	}
 
-	if cfg.TxConnectTimeOut < 0 || cfg.TxConnectTimeOut >= 1 {
+	if cfg.TxConnectTimeOut == 0 {
+		cfg.TxConnectTimeOut = DefaultTxConnectTimeOut
+	}
+	if cfg.TxConnectTimeOut <= 0 || cfg.TxConnectTimeOut >= 1 {
 		str := "%s: The TxConnectTimeOut is out of range (0, 1) " +
-			"-- parsed [%d]"
+			"-- parsed [%f]"
 		err := fmt.Errorf(str, funcName, cfg.TxConnectTimeOut)
 		fmt.Fprintln(os.Stderr, err)
 		fmt.Fprintln(os.Stderr, usageMessage)
 		return nil, nil, err
 	}
-	if cfg.TxConnectTimeOut == 0 {
-		cfg.TxConnectTimeOut = DefaultTxConnectTimeOut
-	}
 
-	if cfg.UtxoValidateTimeOut < 0 || cfg.UtxoValidateTimeOut >= 1 {
+	if cfg.UtxoValidateTimeOut == 0 {
+		cfg.UtxoValidateTimeOut = DefaultUtxoValidateTimeOut
+	}
+	if cfg.UtxoValidateTimeOut <= 0 || cfg.UtxoValidateTimeOut >= 1 {
 		str := "%s: The UtxoValidateTimeOut is out of range (0, 1) " +
-			"-- parsed [%d]"
+			"-- parsed [%f]"
 		err := fmt.Errorf(str, funcName, cfg.UtxoValidateTimeOut)
 		fmt.Fprintln(os.Stderr, err)
 		fmt.Fprintln(os.Stderr, usageMessage)
 		return nil, nil, err
-	}
-	if cfg.UtxoValidateTimeOut == 0 {
-		cfg.UtxoValidateTimeOut = DefaultUtxoValidateTimeOut
 	}
 
 	// Limit the max orphan count to a sane vlue.
